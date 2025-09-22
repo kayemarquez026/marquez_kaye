379e927a91a380e277c4a346c1ee448e4f9d0c53
Kaye Marquez <kayemarquez026@gmail.com>
2025-09-22 10:46:09 +0800

    First commit

--- app/config/config.php
+++ app/config/config.php
@@ -64,7 +64,7 @@ $config['VERSION']                 = '4.2.4';
 | -------------------------------------------------------------------
 | Values: development and production
 */
-$config['ENVIRONMENT']             ='production';
+$config['ENVIRONMENT']             ='development';
 
 /*
 |--------------------------------------------------------------------------
@@ -105,7 +105,7 @@ $config['index_page']               = 'index.php';
 |   3 = All
 |
 */
-$config['log_threshold']            = 0;
+$config['log_threshold']            = 2;
 $config['log_dir']                  = 'runtime/logs/';
 
 /*
