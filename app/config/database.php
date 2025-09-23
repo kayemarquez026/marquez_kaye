4b14446d716a494060c8b46ab4c9b41aad55211b
Kaye Marquez <kayemarquez026@gmail.com>
2025-09-23 14:31:02 +0800

    m

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'info',
+    'username'	=> 'sql12799576',
+    'password'	=> '4qxxuMp3hL',
+    'database'	=> 'sql12799576',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
